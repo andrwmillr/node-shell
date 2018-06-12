aed105e8e9d5828f020391fc76c2ad1bdf2d3203
Andrew Miller <admiller9@gmail.com>
2018-06-12 13:01:09 -0400

    added cat

---
+++ bash.js
@@ -0,0 +1,19 @@
+const pwd = require('./pwd.js');
+const ls = require('./ls.js');
+
+// output a prompt
+process.stdout.write('prompt > ');
+
+// fires on line input
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+  switch (cmd) {
+    case 'pwd': pwd();
+    break;
+    case 'ls': ls();
+    break;
+    case 'cat': cat(file);
+    break;
+    default: console.log('bad command');
+  }
+});
